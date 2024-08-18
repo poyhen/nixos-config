7e3319be195ef8a5f6b86bb41af8d3cec31a6876
poyhen <poyhen@proton.me>
2024-08-18 15:11:42 +0300

    enable chaotic nyx

--- flake.nix
+++ flake.nix
@@ -1,12 +1,15 @@
 {
   description = "A simple NixOS flake";
 
-  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+  };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, chaotic, ... }@inputs: {
     nixosConfigurations.arce = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [ ./hosts/arce/configuration.nix ];
+      modules = [ ./hosts/arce/configuration.nix chaotic.nixosModules.default ];
     };
   };
 }
