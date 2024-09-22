bce0b59d1edce089199b0577deabd48036e32707
poyhen <poyhen@proton.me>
2024-09-22 15:39:56 +0300

    tidy

--- flake.nix
+++ flake.nix
@@ -1,13 +1,9 @@
 {
-  description = "A simple NixOS flake";
+  description = "My Systems Flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
-    nixos-cosmic = {
-      url = "github:lilyinstarlight/nixos-cosmic";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     nix-flatpak.url = "github:gmodena/nix-flatpak";
   };
 
@@ -15,7 +11,6 @@
     {
       nixpkgs,
       chaotic,
-      nixos-cosmic,
       nix-flatpak,
       ...
     }@inputs:
@@ -25,15 +20,6 @@
         modules = [
           ./hosts/arce/configuration.nix
           chaotic.nixosModules.default
-          {
-            nix.settings = {
-              substituters = [ "https://cosmic.cachix.org/" ];
-              trusted-public-keys = [
-                "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
-              ];
-            };
-          }
-          nixos-cosmic.nixosModules.default
           nix-flatpak.nixosModules.nix-flatpak
         ];
       };
--- hosts/arce/configuration.nix
+++ hosts/arce/configuration.nix
@@ -1,8 +1,4 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 
 let
   monitorsXmlContent = builtins.readFile ./monitors.xml;
@@ -10,68 +6,73 @@ let
 in
 {
   imports = [
-    # Include the results of the hardware scan.
     ./hardware-configuration.nix
   ];
 
+  #monitors stuff
   systemd.tmpfiles.rules = [ "L+ /run/gdm/.config/monitors.xml - - - - ${monitorsConfig}" ];
 
+  #nix
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+  nixpkgs.config.allowUnfree = true;
+  system.switch = {
+    enable = false;
+    enableNg = true;
+  };
+  systemd.services.nix-daemon = {
+    environment.TMPDIR = "/var/tmp";
+  };
+
+  #boot and kernel
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.timeout = 2;
+  boot.initrd.systemd.enable = true;
+  boot.tmp.useTmpfs = true;
+  boot.tmp.tmpfsSize = "100%";
   boot.kernelPackages = pkgs.linuxPackages_cachyos;
   chaotic.scx.enable = true;
 
-  systemd.services.scx = {
-    serviceConfig = {
-      StandardError = lib.mkForce "null";
-    };
-  };
-
+  #system
+  networking.networkmanager.enable = true;
+  networking.hostName = "arce";
   networking.firewall.checkReversePath = false;
+  networking.nftables.enable = true;
+  networking.networkmanager.wifi.backend = "iwd";
 
   zramSwap.enable = true;
+  services.fstrim.enable = true;
+  services.dbus.implementation = "broker";
+  services.printing.enable = true;
+
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver = {
+    xkb.layout = "us";
+    xkb.variant = "";
+  };
 
   nixpkgs.config.packageOverrides = pkgs: {
     intel-vaapi-driver = pkgs.intel-vaapi-driver.override { enableHybridCodec = true; };
   };
   hardware.graphics = {
-    # hardware.graphics on unstable
     enable = true;
     extraPackages = with pkgs; [
-      intel-media-driver # LIBVA_DRIVER_NAME=iHD
-      intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      intel-media-driver
+      intel-vaapi-driver
       libvdpau-va-gl
     ];
   };
   environment.sessionVariables = {
     LIBVA_DRIVER_NAME = "iHD";
-  }; # Force intel-media-driver
+  };
 
-  #Enable Flakes
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
-
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.timeout = 2;
-
-  networking.hostName = "arce"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
   time.timeZone = "Europe/Istanbul";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "tr_TR.UTF-8";
     LC_IDENTIFICATION = "tr_TR.UTF-8";
@@ -84,26 +85,6 @@ in
     LC_TIME = "tr_TR.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  #services.desktopManager.cosmic.enable = true;
-  #services.displayManager.cosmic-greeter.enable = true;
-
-  # Configure keymap in X11
-  services.xserver = {
-    xkb.layout = "us";
-    xkb.variant = "";
-  };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -111,22 +92,46 @@ in
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  services.gnome.gnome-remote-desktop.enable = true;
+  security.pam.loginLimits = [
+    {
+      domain = "*";
+      item = "memlock";
+      type = "-";
+      value = "-1";
+    }
+  ];
+
+  #my system/user and packages
+  environment.systemPackages = with pkgs; [
+    dive
+    podman-tui
+    podman-compose
+  ];
+
+  virtualisation.containers.enable = true;
+  virtualisation = {
+    podman = {
+      enable = true;
+      dockerCompat = true;
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
+
+  programs.fish.enable = true;
+  programs.fish.shellAliases = {
+    ls = "lsd -al";
+    cat = "bat";
+  };
   programs.direnv.enable = true;
   programs.direnv.enableFishIntegration = true;
+  programs.starship.enable = true;
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  services.gnome.gnome-remote-desktop.enable = true;
+  programs.firefox.enable = true;
+
   users.mutableUsers = false;
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.frkn = {
     isNormalUser = true;
     description = "frkn";
@@ -190,113 +195,31 @@ in
     ];
   };
 
-  programs.starship.enable = true;
-
-  security.pam.loginLimits = [
-    {
-      domain = "*";
-      item = "memlock";
-      type = "-";
-      value = "-1";
-    }
-  ];
-
-  programs.fish.enable = true;
-  programs.fish.shellAliases = {
-    ls = "lsd -al";
-    cat = "bat";
-  };
-
-  # Install firefox.
-  programs.firefox.enable = true;
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  virtualisation.containers.enable = true;
-  virtualisation = {
-    podman = {
-      enable = true;
-
-      # Create a `docker` alias for podman, to use it as a drop-in replacement
-      dockerCompat = true;
-
-      # Required for containers under podman-compose to be able to talk to each other.
-      defaultNetwork.settings.dns_enabled = true;
-    };
-  };
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    #  wget
-    dive
-    podman-tui
-    podman-compose
-  ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
-
-  # experimental stuff
-  networking.nftables.enable = true;
-
-  system.switch = {
-    enable = false;
-    enableNg = true;
-  };
-
-  boot.initrd.systemd.enable = true;
-
-  networking.networkmanager.wifi.backend = "iwd";
-
-  boot.tmp.useTmpfs = true;
-  boot.tmp.tmpfsSize = "100%";
-
-  systemd.services.nix-daemon = {
-    environment.TMPDIR = "/var/tmp";
-  };
-
-  services.fstrim.enable = true;
-
-  services.dbus.implementation = "broker";
-
+  #flatpak
+  services.flatpak.enable = true;
   xdg.portal.enable = true;
   xdg.portal.xdgOpenUsePortal = true;
-  services.flatpak.enable = true;
-
+  services.flatpak.update.onActivation = true;
+  services.flatpak.remotes = [
+    {
+      name = "flathub";
+      location = "https://flathub.org/repo/flathub.flatpakrepo";
+    }
+    {
+      name = "flathub-beta";
+      location = "https://flathub.org/beta-repo/flathub-beta.flatpakrepo";
+    }
+  ];
   services.flatpak.packages = [
-    "com.brave.Browser"
-    "com.ticktick.TickTick"
+    {
+      appId = "com.brave.Browser";
+      origin = "flathub-beta";
+    }
     "de.haeckerfelix.Fragments"
     "org.telegram.desktop"
   ];
 
-  services.flatpak.update.onActivation = true;
+  #yes
+  system.stateVersion = "24.05";
 
 }
