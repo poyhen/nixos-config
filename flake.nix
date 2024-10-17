95078c88fbec60934c0d5840ae05dfa131a207dc
poyhen <poyhen@proton.me>
2024-10-17 21:02:11 +0300

    use patched code-cursor to remove double headerbar

--- flake.nix
+++ flake.nix
@@ -1,19 +1,19 @@
 {
   description = "My Systems Flake";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
     nix-flatpak.url = "github:gmodena/nix-flatpak";
     nixos-cosmic.url = "github:lilyinstarlight/nixos-cosmic";
+    custom-packages.url = "github:poyhen/custom-packages";
   };
-
   outputs =
     {
       nixpkgs,
       chaotic,
       nix-flatpak,
       nixos-cosmic,
+      custom-packages,
       ...
     }:
     {
@@ -30,6 +30,16 @@
           ./hosts/arce/configuration.nix
           chaotic.nixosModules.default
           nix-flatpak.nixosModules.nix-flatpak
+          (
+            { pkgs, ... }:
+            {
+              nixpkgs.overlays = [
+                (final: prev: {
+                  code-cursor = custom-packages.packages.${pkgs.system}.code-cursor;
+                })
+              ];
+            }
+          )
         ];
       };
     };
